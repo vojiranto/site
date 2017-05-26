d84d0255c50df88cc364e57636b5816789798d54
al.lastor <al.lastor@bk.ru>
2017-05-26 22:31:30 +0300

    v.0.0.101

--- site.hs
+++ site.hs
@@ -7,6 +7,11 @@ import           Data.Time
 --------------------------------------------------------------------------------
 main :: IO ()
 main = hakyll $ do
+{-
+    match "images/*" $ do
+        route   idRoute
+        compile copyFileCompiler
+-}
     match "css/*" $ do
         route   idRoute
         compile compressCssCompiler
