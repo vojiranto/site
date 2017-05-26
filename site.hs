b01694053465d56c2b7ca6c678d33937cac0415c
al.lastor <al.lastor@bk.ru>
2017-05-26 18:22:54 +0300

    v.0.0.29

--- site.hs
+++ site.hs
@@ -2,7 +2,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 import           Data.Monoid (mappend)
 import           Hakyll
-
+import           Data.Time.Format.Locale
 
 --------------------------------------------------------------------------------
 main :: IO ()
