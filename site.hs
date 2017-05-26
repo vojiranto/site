e0e1d0eed30417ad1dbc57472448326eb77002d3
al.lastor <al.lastor@bk.ru>
2017-05-26 19:20:17 +0300

    v.0.0.51

--- site.hs
+++ site.hs
@@ -49,7 +49,7 @@ main = hakyll $ do
             posts <- recentFirst =<< loadAll "posts/*"
             let indexCtx =
                     listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Основная"            `mappend`
+                    constField "title" "Главная"            `mappend`
                     defaultContext
 
             getResourceBody
