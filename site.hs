518d23bc22dcbc86e2efb4d936a74327e416cd4b
al.lastor <al.lastor@bk.ru>
2017-05-27 15:21:13 +0300

    v.0.0.289

--- site.hs
+++ site.hs
@@ -13,7 +13,7 @@ main = hakyll $ do
 
     match "css/*.hs" $ do
         route   $ setExtension "css"
-        compile $ getResourceString >>= withItemBody (unixFilter "runghc" [])
+        compile $ getResourceString >>= withItemBody (unixFilter "stack exec runghc" [])
 {-
     match "css/*.css" $ do
         route   idRoute
