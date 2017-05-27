5c637e7d8e4917ca842554262bd47c1c2c9ac7c1
al.lastor <al.lastor@bk.ru>
2017-05-27 17:23:05 +0300

    v.0.0.312

--- site.hs
+++ site.hs
@@ -115,7 +115,7 @@ defaultCss = render $ do
         background    white
         borderRadius (px 5) (px 5) (px 5) (px 5)
         fontSize     (px 20)
-        padding      (px 30) (px 20) (px 0) (px 0)   
+        padding      (px 30) (px 0) (px 20) (px 0)   
 
     div # "#content" ? h1 ? do
         borderBottom solid (px 2) black
